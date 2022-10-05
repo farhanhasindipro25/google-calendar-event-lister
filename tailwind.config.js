186380eb90fd0c33e46a6854a363091c511c0456
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-10-05 19:56:01 +0600

    front-end code done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        auto1: "auto 1fr",
+      },
+      gridTemplateColumns: {
+        cards: "repeat(auto-fit, minmax(250px, 1fr))",
+      },
+    },
   },
   plugins: [],
-}
+};
